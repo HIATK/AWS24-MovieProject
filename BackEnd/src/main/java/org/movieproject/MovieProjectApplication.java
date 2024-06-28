45f62b65c7920959f7f7be93e7d7c6a3146d1adf
scsc23 <xogmlm0507@gmail.com>
2024-06-28 15:25:33 +0900

    security

--- BackEnd/src/main/java/org/movieproject/MovieProjectApplication.java
+++ BackEnd/src/main/java/org/movieproject/MovieProjectApplication.java
@@ -1,4 +1,4 @@
-package org.crm.movieproject;
+package org.movieproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ BackEnd/src/main/java/org/movieproject/dto/MemberDTO.java
@@ -0,0 +1,18 @@
+package org.movieproject.dto;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class MemberDTO {
+    private Integer memberNo;
+
+    private String memberEmail;
+
+    private String memberPw;
+
+    private String memberName;
+
+    private String memberPhone;
+}
