61eb7b8bd25b6399bbf0169908a2849677cb3980
WATCHHONG <gihong0101@gmail.com>
2024-07-03 12:25:56 +0900

    join login 정리1 / domain / Member 에 오류나요

--- backEnd/src/main/java/org/movieproject/service/MemberServiceImpl.java
+++ backEnd/src/main/java/org/movieproject/service/MemberServiceImpl.java
@@ -21,7 +21,6 @@ public class MemberServiceImpl implements MemberService {
     private final MemberRepository memberRepository;
     private final ModelMapper modelMapper;
     private final PasswordEncoder passwordEncoder;
-    private final JwtProvider jwtProvider;
 
     // 회원가입
     @Override
@@ -34,6 +33,7 @@ public class MemberServiceImpl implements MemberService {
         if (exist) {
             throw new MemberService.MidExistException("아이디가 중복되었습니다 !!! ");
         }
+
         // 비밀번호 암호화
         String encodePassword = passwordEncoder.encode(memberDTO.getMemberPw());
         memberDTO.setMemberPw(encodePassword);
@@ -45,31 +45,5 @@ public class MemberServiceImpl implements MemberService {
 
         memberRepository.save(member);
 
-        // JWT 토큰 생성
-        Map<String, Object> tokenData = new HashMap<>();
-        tokenData.put("email", member.getMemberEmail());
-
-        // 만료시간 60분
-        String token = jwtProvider.generateToken(tokenData, 60);
-
-        log.info("JWT 토큰 생성 !!! {}", token);
-    }
-
-    // 회원 이메일 참조하여 DTO 반환하는 서비스
-    @Override
-    @Transactional(readOnly = true)
-    public MemberDTO findMemberByEmail(String memberEmail){
-        Member member = memberRepository.FindByMemberEmailWithRoles(memberEmail)
-                .orElseThrow(() -> new RuntimeException("해당 이메일의 멤버를 찾을 수 없어요 !!!"));
-        return convertToDTO(member);
-    }
-    private MemberDTO convertToDTO(Member member){
-        MemberDTO memberDTO = new MemberDTO();
-        memberDTO.setMemberEmail(member.getMemberEmail());
-        memberDTO.setMemberPw(member.getMemberPw());
-        memberDTO.setMemberName(member.getMemberName());
-        memberDTO.setMemberPhone(member.getMemberPhone());
-        memberDTO.setMemberNick(member.getMemberNick());
-        return memberDTO;
     }
 }
