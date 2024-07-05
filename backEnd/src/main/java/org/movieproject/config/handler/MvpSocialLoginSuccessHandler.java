124c4070965cbaa2a3900cbd58a3292364982bcb
scsc23 <xogml0507@gmail.com>
2024-07-05 11:29:47 +0900

    work

--- backEnd/src/main/java/org/movieproject/config/handler/MvpSocialLoginSuccessHandler.java
+++ backEnd/src/main/java/org/movieproject/config/handler/MvpSocialLoginSuccessHandler.java
@@ -1,22 +1,24 @@
 package org.movieproject.config.handler;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.movieproject.member.dto.MemberSecurityDTO;
-import org.movieproject.security.util.JwtLoginUtil;
+import org.movieproject.security.JwtProvider;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 import java.io.IOException;
+import java.util.Map;
 
 @Log4j2
 @RequiredArgsConstructor
 public class MvpSocialLoginSuccessHandler implements AuthenticationSuccessHandler {
 
-    private final JwtLoginUtil jwtLoginUtil;
+    private final JwtProvider jwtProvider;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
@@ -25,15 +27,32 @@ public class MvpSocialLoginSuccessHandler implements AuthenticationSuccessHandle
 
         MemberSecurityDTO memberSecurityDTO = (MemberSecurityDTO) authentication.getPrincipal();
 
-        // JWT 생성 및 전송
-        jwtLoginUtil.generateAndSendTokens(response, authentication);
+        Map<String, Object> claim = Map.of(
+                "username", authentication.getName(),
+                "authority", authentication.getAuthorities()
+        );
+
+        String accessToken = jwtProvider.generateToken(claim, 5);
+        String refreshToken = jwtProvider.generateToken(claim, 10);
+
+        Cookie accessTokenCookie = new Cookie("accessToken", accessToken);
+        Cookie refreshTokenCookie = new Cookie("refreshToken", refreshToken);
+
+        accessTokenCookie.setPath("/");
+        refreshTokenCookie.setPath("/");
+
+        accessTokenCookie.setMaxAge(60);
+        refreshTokenCookie.setMaxAge(60);
+
+        response.addCookie(accessTokenCookie);
+        response.addCookie(refreshTokenCookie);
 
         // 임시 비밀번호 사용자 여부에 따라 다른 URL로 리디렉트
         if (memberSecurityDTO.isSocial() && memberSecurityDTO.getMemberPw().length() == 8) {
             log.info("임시 비밀번호 사용자");
-            response.sendRedirect("http://localhost:3000/");
+            response.sendRedirect("http://localhost:3000/member/login");
         } else {
-            response.sendRedirect("http://localhost:3000/");
+            response.sendRedirect("http://localhost:3000/member/login");
         }
     }
 }
