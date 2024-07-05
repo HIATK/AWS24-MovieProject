f2d62e599932bdebd2df38d42d3210b3afd89c13
scsc23 <xogml0507@gmail.com>
2024-07-05 13:17:58 +0900

    work

--- frontend/app/member/login/page.tsx
+++ frontend/app/member/login/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import React, { useState } from 'react';
-import Login from '@/app/(components)/Member/Login'; // 실제 경로에 맞게 수정 필요
-import styles from "@/app/page.module.css";
+import Login from '@/(components)/Member/Login'; // 실제 경로에 맞게 수정 필요
+import styles from "@/page.module.css";
 
 export default function login(){
 
