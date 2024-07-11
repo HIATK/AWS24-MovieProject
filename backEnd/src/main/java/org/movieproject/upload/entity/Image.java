35966138beb2ed1a0a73a4382018fc09021ef4b2
scsc23 <xogmlm0507@gmail.com>
2024-07-11 22:19:05 +0900

    work

--- backEnd/src/main/java/org/movieproject/upload/entity/Image.java
+++ backEnd/src/main/java/org/movieproject/upload/entity/Image.java
@@ -1,28 +1,28 @@
-package org.movieproject.upload.entity;
+    package org.movieproject.upload.entity;
 
-import jakarta.persistence.*;
-import lombok.*;
-import org.movieproject.member.entity.Member;
+    import jakarta.persistence.*;
+    import lombok.*;
+    import org.movieproject.member.entity.Member;
 
-@Entity
-@Builder
-@Getter
-@AllArgsConstructor
-@NoArgsConstructor
-@ToString(exclude = "member")
-public class Image{
+    @Entity
+    @Builder
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @ToString(exclude = "member")
+    public class Image{
 
-    @Id
-    private String uuid;
+        @Id
+        private String uuid;
 
-    private String filePath;
+        private String filePath;
 
-    //  프로필 이미지는 회원 당 1개만 가질 수 있으므로 @OneToOne 적용
-    @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "memberNo")
-    private Member member;
+        //  프로필 이미지는 회원 당 1개만 가질 수 있으므로 @OneToOne 적용
+        @OneToOne(fetch = FetchType.LAZY)
+        @JoinColumn(name = "memberNo")
+        private Member member;
 
-    //  프로필 이미지 수정
-    public void changeImage(String filePath){this.filePath = filePath;}
+        //  프로필 이미지 수정
+        public void changeImage(String filePath){this.filePath = filePath;}
 
-}
+    }
