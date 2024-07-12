3d7d9134a2c366641802f2dee54f0b37cc1f2f88
WATCHHONG <gihong0101@gmail.com>
2024-07-12 15:45:10 +0900

    ..

--- backEnd/src/main/java/org/movieproject/member/controller/MemberController.java
+++ backEnd/src/main/java/org/movieproject/member/controller/MemberController.java
@@ -2,20 +2,20 @@ package org.movieproject.member.controller;
 
 import io.jsonwebtoken.JwtException;
 import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.movieproject.like.entity.Like;
-import org.movieproject.member.entity.Member;
 import org.movieproject.member.dto.MemberDTO;
+import org.movieproject.member.entity.Member;
 import org.movieproject.member.repository.MemberRepository;
 import org.movieproject.member.service.MemberService;
 import org.movieproject.security.JwtProvider;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -47,7 +47,7 @@ public class MemberController {
             memberService.memberJoin(memberDTO);
         } catch (MemberService.MidExistException e) {
             return ResponseEntity.badRequest().body("중복된 아이디 입니다 !!!");
-            }
+        }
         return ResponseEntity.ok("회원가입에 성공하였습니다 !!!");
     }
 
@@ -78,7 +78,54 @@ public class MemberController {
         return member.orElseThrow().getLikeMovies();
     }
 
-    // 회원 정보 업데이트
+    // 회원정보 수정 / 비밀번호 검증
+    @PostMapping("/verifyPw")
+    public ResponseEntity<Map<String, Object>> verifyPassword(@RequestBody Map<String, String> request) {
+        log.info("리퀘스트 !!! : " + request);
+        try {
+            String username = SecurityContextHolder.getContext().getAuthentication().getName();
+
+            log.info("프린서펄 값 !!!: {}", username);
+
+            Optional<Member> memberOptional = memberRepository.findByMemberEmailWithRoles(username);
+
+            log.info("멤버 옵셔널 !!! : " + memberOptional);
+            if (memberOptional.isPresent()) {
+                Member member = memberOptional.get();
+                log.info("멤버 !!! : "+ member);
+                boolean isPasswordValid = passwordEncoder.matches(request.get("password"), member.getMemberPw());
+                log.info("isPasswordValid !!! : " + isPasswordValid);
+                return ResponseEntity.ok(Map.of("isValid", (Object) isPasswordValid));
+            } else {
+                return ResponseEntity.badRequest().body(Map.of("error", "사용자를 찾을 수 없습니다."));
+            }
+        } catch (JwtException e) {
+            return ResponseEntity.badRequest().body(Map.of("error", "Invalid token"));
+        }
+    }
+
+//    // 회원정보 수정 / 중복된 닉네임 체크
+//    @PostMapping("/verifyNick")
+//    public ResponseEntity<Map<String, Object>> verifyNick(@RequestBody Map<String, String> request) {
+//        log.info("리퀘스트 !!! : " + request);
+//        try {
+//
+//            if (memberOptional.isPresent()) {
+//                Member member = memberOptional.get();
+//                log.info("멤버 !!! : "+ member);
+//                boolean isPasswordValid = passwordEncoder.matches(request.get("password"), member.getMemberPw());
+//                log.info("isPasswordValid !!! : " + isPasswordValid);
+//                return ResponseEntity.ok(Map.of("isValid", (Object) isPasswordValid));
+//            } else {
+//                return ResponseEntity.badRequest().body(Map.of("error", "사용자를 찾을 수 없습니다."));
+//            }
+//        } catch (JwtException e) {
+//            return ResponseEntity.badRequest().body(Map.of("error", "Invalid token"));
+//        }
+//    }
+
+
+    // 회원정보 수정
     @PutMapping("/update")
     public ResponseEntity<?> update(@Valid @RequestBody MemberDTO memberDTO) {
         log.info("회원 정보 업데이트 시작 !!!"+memberDTO);
@@ -99,35 +146,18 @@ public class MemberController {
     }
 
     @GetMapping("/check_auth")
-    public ResponseEntity<?> checkAuth() {
+    public ResponseEntity<Object> checkAuth() {
         try {
             Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-            if (authentication == null || !authentication.isAuthenticated() || "anonymousUser".equals(authentication.getPrincipal())) {
+            if (authentication == null || !authentication.isAuthenticated()) {
                 return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("인증되지 않은 사용자입니다.");
             }
 
-            String username = null;
-            if (authentication.getPrincipal() instanceof UserDetails userDetails) {
-                username = userDetails.getUsername(); // 일반적으로 이메일이나 ID를 반환
-            }
-
-            Set<String> roles = authentication.getAuthorities().stream()
+            Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+            Set<String> roles = authorities.stream()
                     .map(grantedAuthority -> grantedAuthority.getAuthority().replace("ROLE_", ""))
                     .collect(Collectors.toSet());
-
-            String memberNick = null;
-            if (username != null) {
-                Member member = memberRepository.findByMemberEmail(username)
-                        .orElseThrow(() -> new RuntimeException("Member not found"));
-                memberNick = member.getMemberNick();
-            }
-
-            Map<String, Object> authInfo = new HashMap<>();
-            authInfo.put("roles", roles);
-            authInfo.put("memberNick", memberNick);
-            log.info("멤버닉 서버에서 보낸다: " + memberNick);
-
-            return ResponseEntity.ok(authInfo);
+            return ResponseEntity.ok(roles);
 
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("체크어쓰 실패");
